b09e8472b7a822f4f491aec5d42cd18f3c65d407
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-10-25 18:31:27 +0300

    Added header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,61 @@ import styles from './Header.module.scss';
 import Icons from '../Icons';
 
 const Header = () => {
+  const [menu, setMenu] = React.useState(true);
+
+  const onClickBurger = () => {
+    setMenu(!menu);
+  };
+
+  React.useEffect(() => {
+    if (window.screen.width <= 766) {
+      setMenu(false);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (menu) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'visible';
+    }
+  }, [menu]);
+
   return (
-    <div className={styles.wrapper}>
+    <header className={styles.header}>
       <div className="container">
-        header
-        <Icons name="traders" />
+        <div className={styles.wrapper}>
+          <a className={styles.logo} href="/#">
+            <Icons name="logo" />
+          </a>
+          <div className={styles.burger} onClick={onClickBurger}></div>
+          {menu && (
+            <div className={styles.content}>
+              <nav>
+                <ul className={styles.menu}>
+                  <li>
+                    <a href="/#">Activities</a>
+                  </li>
+                  <li>
+                    <a href="/#">Technology</a>
+                  </li>
+                  <li>
+                    <a href="/#">R&D</a>
+                  </li>
+                  <li>
+                    <a href="/#">Community</a>
+                  </li>
+                </ul>
+              </nav>
+              <a className={styles.btn} href="/#">
+                <span>Career</span>
+                <Icons name="arrow-right-up" />
+              </a>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 
